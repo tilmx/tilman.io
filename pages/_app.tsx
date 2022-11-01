27cfea182175739ab800fbcd114574321f91702c
Tilman Frick <git@tilman.io>
2022-11-01 19:28:59 +0100

    fix: clean up files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,9 @@ function App({ Component, pageProps }: AppProps) {
     <Theme>
       <Head>
         <link rel="icon" href="/favicon.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
+        <meta name="language" content="en" />
+        <meta name="referrer" content="no-referrer" />
       </Head>
       <Global
         styles={css`
