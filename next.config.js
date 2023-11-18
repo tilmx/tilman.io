cf25345669978dfb508aebc119424ff8cddd409e
Tilman Frick <git@tilman.io>
2023-11-18 18:33:05 +0100

    chore: extend image sizes

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   images: {
     loader: "custom",
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2560],
   },
   transpilePackages: ["next-image-export-optimizer"],
   env: {
