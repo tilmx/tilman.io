e5be4238f44713744852f7e06f28b87f802b6953
Tilman Frick <git@tilman.io>
2023-11-19 01:35:41 +0100

    chore: remove smaller image sizes

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   output: 'export',
   images: {
     loader: "custom",
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    imageSizes: [64, 96, 128, 256, 384],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2560],
   },
   transpilePackages: ["next-image-export-optimizer"],
