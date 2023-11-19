03f7cfb14e1ddf8ed5dea4f06a8302eef0d07ef1
Tilman Frick <git@tilman.io>
2023-11-19 14:07:57 +0100

    feat: redesign home screen further

--- components/tokens/colors.tsx
+++ components/tokens/colors.tsx
@@ -5,7 +5,6 @@ import React from 'react';
 export enum Color {
     Text = 'var(--color-text)',
     Background = 'var(--color-background)',
-    Accent = 'var(--color-accent)',
 }
 
 export const Theme: React.FunctionComponent<{ children?: React.ReactNode }> = props => {
@@ -13,14 +12,13 @@ export const Theme: React.FunctionComponent<{ children?: React.ReactNode }> = pr
         <>
             <Global styles={css`
                 :root {
-                    --color-text: #000000;
-                    --color-background: #FFFFFF;
-                    --color-accent: #0C2EF9;
+                    --color-text: #FFFFFF;
+                    --color-background: #000000;
 
-                    @media (prefers-color-scheme: dark) {
-                        --color-text: #FFFFFF;
-                        --color-background: #000000;
-                    }
+                    /*@media (prefers-color-scheme: dark) {
+                        --color-text: #000000;
+                        --color-background: #FFFFFF;
+                    }*/
                 }
             `} />
             {props.children}
